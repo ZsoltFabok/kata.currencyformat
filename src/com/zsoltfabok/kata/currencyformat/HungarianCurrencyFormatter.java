45e02939748824bc7974a17d2ee07f566de54e2d
Zsolt Fabok <me@zsoltfabok.com>
2011-02-06 12:40:51 +0100

    This is where Tamas Gyorfi and I finished our Kata session.
    The following items are still missing:
     - refactoring the actual code
     - handle decimal mark

--- src/com/zsoltfabok/kata/currencyformat/HungarianCurrencyFormatter.java
+++ src/com/zsoltfabok/kata/currencyformat/HungarianCurrencyFormatter.java
@@ -1,5 +1,15 @@
 package com.zsoltfabok.kata.currencyformat;
 
 public class HungarianCurrencyFormatter {
+    public String format(String number) {
+        final StringBuilder formattedNumber = new StringBuilder(number);
 
+        for (int i = 1; i <= (number.length() / 3); i++) {
+            int position = number.length() - 3 * i;
+            if (position != 0) {
+                formattedNumber.insert(position, " ");
+            }
+        }
+        return formattedNumber.toString();
+    }
 }
