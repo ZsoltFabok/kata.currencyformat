2ae44d982cffc0233a2e02aaa1959c718f086601
Zsolt Fabok <me@zsoltfabok.com>
2011-02-06 23:24:43 +0100

    Solved with locale based currency number formatting

--- src/com/zsoltfabok/kata/currencyformat/HungarianCurrencyFormatter.java
+++ src/com/zsoltfabok/kata/currencyformat/HungarianCurrencyFormatter.java
@@ -1,5 +1,31 @@
 package com.zsoltfabok.kata.currencyformat;
 
-public class HungarianCurrencyFormatter {
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 
+public class HungarianCurrencyFormatter {
+    private static Locale HUN = new Locale("hu", "HU");
+
+    public String format(String number) {
+        NumberFormat currencyFormatter =
+            NumberFormat.getCurrencyInstance(HUN);
+
+        String currencyFormattedNumber =
+            currencyFormatter.format(Double.parseDouble(number));
+
+        String formattedNumberWithProperGroupSeparator =
+            replaceLocaleSpecificGroupSeparatorWithSpace(currencyFormattedNumber);
+
+        return removeFtSuffix(formattedNumberWithProperGroupSeparator);
+    }
+
+    private String removeFtSuffix(String number) {
+        return number.substring(0, number.indexOf(" Ft"));
+    }
+
+    private String replaceLocaleSpecificGroupSeparatorWithSpace(String number) {
+        char separator = DecimalFormatSymbols.getInstance(HUN).getGroupingSeparator();
+        return number.replace(separator, ' ');
+    }
 }
--- test/com/zsoltfabok/kata/currencyformat/HungarianCurrencyFormatterTest.java
+++ test/com/zsoltfabok/kata/currencyformat/HungarianCurrencyFormatterTest.java
@@ -1,6 +1,56 @@
 package com.zsoltfabok.kata.currencyformat;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class HungarianCurrencyFormatterTest {
+    private HungarianCurrencyFormatter formatter;
 
+    @Before
+    public void setUp() {
+        formatter = new HungarianCurrencyFormatter();
+    }
+
+    @Test
+    public void shouldReturnTenHungarianFormat() {
+        assertEquals("10", formatter.format("10"));
+    }
+
+    @Test
+    public void shouldReturnOneHundredInHungarianFormat() {
+        assertEquals("100", formatter.format("100"));
+    }
+
+    @Test
+    public void shouldReturnTwoThousandInHungarianFormat() {
+        final String actualResult = formatter.format("2000");
+        assertEquals("2 000", actualResult);
+    }
+
+    @Test
+    public void shouldReturnTwentyThousandInHungarianFormat() {
+        assertEquals("20 000", formatter.format("20000"));
+    }
+
+    @Test
+    public void shouldReturnHundredThousandInHungarianFormat() {
+        assertEquals("100 000", formatter.format("100000"));
+    }
+
+    @Test
+    public void shouldReturnOneMillionInHungarianFormat() {
+        assertEquals("1 000 000", formatter.format("1000000"));
+    }
+
+    @Test
+    public void shouldReturnTenPointOneInHungarianFormat() {
+        assertEquals("10,1", formatter.format("10.1"));
+    }
+
+    @Test
+    public void shouldReturnThousandPointOneInHungarianFormat() {
+        assertEquals("1 000,1", formatter.format("1000.1"));
+    }
 }
